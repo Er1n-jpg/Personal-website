216e340f1fea746353fbdae62f1bff5fad97da47
Erin <enguy4@ocdsb.ca>
2026-01-29 16:42:29 -0500

    let there be sound!!! (also adding transitions)

---
+++ main.js
@@ -0,0 +1,14 @@
+
+const audio1 = document.getElementById("audio")
+const audio2 = document.getElementById("audio2")
+const audio3 = document.getElementById("audio3")
+
+const playlist = [audio1, audio2, audio3];
+let currentTrack = 0;
+
+function displaytrack(){
+    index = 0;
+    if (currentTrack == 0){
+        
+    }
+}
