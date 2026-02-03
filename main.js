a96207bd8bb0716f4a886c702857d2c26e9cd801
Erin <enguy4@ocdsb.ca>
2026-02-02 20:29:46 -0500

    Trying to figure out how to display audio

--- main.js
+++ main.js
@@ -3,12 +3,20 @@ const audio1 = document.getElementById("audio")
 const audio2 = document.getElementById("audio2")
 const audio3 = document.getElementById("audio3")
 
-const playlist = [audio1, audio2, audio3];
+const playlist = [
+    {title: "Kill the romeo", artist: "Zerobaseone"},
+    {title: "Iconick", artist: "Zerobaseone"},
+    {title: "Kidult", artist: "SEVENTEEN"}
+]
 let currentTrack = 0;
+var applicationopen = true;
 
-function displaytrack(){
-    index = 0;
-    if (currentTrack == 0){
-        
-    }
+
+
+function updatesong (index){
+    const song = playlist[index]
+    document.getElementById("Songtitle").textContent = song.title;
+    document.getElementById('artist').textContent = song.artist;
+
 }
+
